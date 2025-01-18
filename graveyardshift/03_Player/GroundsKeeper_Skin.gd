9e4d8d390185b709fc0efe07d9527007e86e9c78
Zazzelz <kafka_clown@hotmail.com>
2025-01-18 22:17:18 +0800

    Added player camera, Idle and walk animations and dynamic lights

---
+++ graveyardshift/03_Player/GroundsKeeper_Skin.gd
@@ -0,0 +1,16 @@
+class_name GroundsKeeper 
+extends Node3D
+
+@onready var animation_tree: AnimationTree = $"../AnimationTree"
+@onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/StateMachine/playback")
+
+
+func idle():
+	state_machine.travel("PlayerAnimations_Idle")
+	
+func walk():
+	state_machine.travel("PlayerAnimations_Walk")
+	
+#func jump():
+	#state_machine.travel("PlayerAnimations_Jump")
+	
