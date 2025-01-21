2df6b7d00ed7786ccd240efa555961e7481900fb
Zazzelz <kafka_clown@hotmail.com>
2025-01-21 16:57:38 +0800

    fixed issues with character not moving

--- graveyardshift/04_Menus/menu_player.gd
+++
@@ -1,9 +0,0 @@
-extends Node3D
-
-@onready var animation_player: AnimationPlayer = $BasePose/AnimationPlayer
-
-
-func _ready():
-	animation_player.play("PlayerAnimations_Walk")
-	animation_player.queue("PlayerAnimations_Thriller01")
-	animation_player.play("PlayerAnimations_Thriller01")
