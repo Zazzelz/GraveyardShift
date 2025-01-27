838f386ed4dcb7eb145606338d92e8e3ed5b346d
Zazzelz <kafka_clown@hotmail.com>
2025-01-27 19:03:19 +0800

    Zombie animations
    starting to build up Zombie animations and pathing

--- graveyardshift/05_Zombies/zombie.gd
+++ graveyardshift/05_Zombies/zombie.gd
@@ -1,22 +1,26 @@
-extends Node3D
+extends CharacterBody3D
 
-@export var stage_path : NodePath
-
-@onready var nav_agent = $NavigationAgent3D
-@onready var animation_tree: AnimationTree = $AnimationTree
-@onready var animation_player: AnimationPlayer = $AnimationPlayer
-
+var player = null
 const SPEED = 4.0
 
+@export var player_path : NodePath
+
+@onready var nav_agent = $NavigationAgent3D
+
+
 func _ready() -> void:
-	stage_marker = get_node(stage_path)
+	player = get_node(player_path)
 
 func _process(delta: float) -> void:
 	velocity = Vector3.ZERO
 	
 	#Navigation
-	nav_agent.set_target_position(stage_marker.global_transform.origin)
+	nav_agent.set_target_position(player.global_transform.origin)
 	
 	var next_nav_point = nav_agent.get_next_path_position()
 	velocity = (next_nav_point - global_transform.origin).normalized() * SPEED
-	distance = global_position.distance_to(stage_marker.global_position)
+	#distance = global_position.distance_to(player.global_position)
+	
+	look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z), Vector3.UP)
+	
+	move_and_slide()
